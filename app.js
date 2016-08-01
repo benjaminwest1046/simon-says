ae4ea376b7769f675b301933d989b979e45e3634
Benjamin West <benjaminwest1046@gmail.com>
2016-08-01 10:05:57 -0400

    Initial commit, rough draft html board, css, and a working JS function.

---
+++ app.js
@@ -0,0 +1,14 @@
+window.onload = function() {
+  /*
+  console.log("something")
+  var topLeft = document.getElementById("top-left");
+  topLeft.addEventListener("click", function() {
+    console.log("Button was clicked");
+  });
+  */
+}
+
+function someFunction() {
+  console.log("This works")
+  }
+
