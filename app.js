62cad74143e1d6a0d57fd50bbe4b7a66e3fa7aab
Benjamin West <benjaminwest1046@gmail.com>
2016-08-01 11:32:16 -0400

    Added javascript to pick a random button and change the color of it

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 window.onload = function() {
-  /*
-  console.log("something")
-  var topLeft = document.getElementById("top-left");
-  topLeft.addEventListener("click", function() {
-    console.log("Button was clicked");
-  });
-  */
-}
 
-function someFunction() {
-  console.log("This works")
+  var board = {
+    name: "Simon-Says",
+    activeButton: "black"
+    buttons: [
+      topRight    = document.getElementById("top-right").id,
+      topLeft     =  document.getElementById("top-left").id,
+      bottomRight = document.getElementById("bottom-right").id,
+      bottomLeft  = document.getElementById("bottom-left").id
+    ],
+    pickButton: function(){
+      var currentButton = board.buttons[Math.floor(Math.random() * this.buttons.length)];
+      console.log("Pick button function = " + '"' + "#" + currentButton + '"')
+      return(currentButton)
+    },
+    changeColor: function() {
+      var buttonToChange = document.getElementById(board.pickButton());
+      console.log("Change color function = " + board.pickButton());
+      buttonToChange.style.background = activeButton;
+    }
   }
 
+  board.changeColor()
+
+}
