3fbaa010bbc2662004ca4e23227b5aab69bf1ec7
Benjamin West <benjaminwest1046@gmail.com>
2016-08-02 14:44:43 -0400

    Added ability to reset class after a loop through a sequence

--- app.js
+++ app.js
@@ -3,25 +3,18 @@ window.onload = function() {
   var board = {
     name: "Simon-Says",
     activeButton: "white",
-    sequenceArrayasd: ["top-left", "top-right"],
     buttons: [
       topRight    = document.getElementById("top-right").id,
       topLeft     =  document.getElementById("top-left").id,
       bottomRight = document.getElementById("bottom-right").id,
       bottomLeft  = document.getElementById("bottom-left").id
-    ]/*,
-    sequence: function() {
-      //var nextButton = board.buttons[Math.floor(Math.random() * board.buttons.length)];
-      //board.sequenceArray.push(nextButton);
-      for (i = 0; i < board.sequenceArray.length; i++) {
-        var ele = document.getElementById(board.sequenceArray[i]);
-        ele.style.background = board.activeButton;
-      }
-    }*/
+    ]
   }
 
 }
-var userClicks = []
+
+var userClicks = [];
+var sequenceArray = ["top-right", "top-left", "bottom-left", "bottom-right"];
 var buttonClicked = document.getElementsByClassName("buttons");
 for (var i = 0; i < buttonClicked.length; i++) {
   buttonClicked[i].addEventListener("click", function(){
@@ -30,72 +23,51 @@ for (var i = 0; i < buttonClicked.length; i++) {
   });
 }
 
-var sequenceArray = ["top-right", "top-left"]
 function checkLoss () {
-  if (userClicks == sequenceArray) {
-    alert("You win ")
-  } else {
-    alert("You lost" + buttonClicked + " " + sequenceArray)
+  for (var i = 0; i < sequenceArray.length; i++) {
+    console.log(userClicks[i] + " sequenceArray = " + sequenceArray[i])
+    if (userClicks[i] != sequenceArray[i]) {
+      alert("You lose");
+    }
   }
-}
-function clearButton() {
-  console.log("Is it geting here")
   console.log(sequenceArray)
+  userClicks = [];
+  setTimeout(function() {
+    playSequence();
+  },100)
+  playSequence();
+
 }
 
-function goToNextButton() {
-  var x = 0;
-  var ele = document.getElementById(sequenceArray[x]);
-  ele.style.background = "white";
-  /*for (i = 0; i < sequenceArray.length; i++) {
-
-    var ele = document.getElementById(sequenceArray[i]);
-        ele.style.background = "white";
-        setInterval(clearButton, 500);*/
-  }
-
-
-function delayedPlaySequenceEnd(button, delay) {
+function delayedPlaySequence(button, delay) {
   var ele = document.getElementById(button);
   setTimeout(function() {
-      ele.className = "elementToFadeInAndOut";
+      ele.classList.add("elementToFadeInAndOut");
   }, delay)
 }
 
 function playSequence() {
+  resetClasses();
   for (var i = 0; i < sequenceArray.length; i++) {
-    delayedPlaySequenceEnd(sequenceArray[i], i * 1000);
+    delayedPlaySequence(sequenceArray[i], i * 1000);
+  }
+  checkLoss();
+}
+
+function resetClasses() {
+  var allBoxes = document.getElementsByClassName("buttons");
+  console.log(allBoxes)
+  for (var i = 0; i < allBoxes.length; i++) {
+    console.log(allBoxes[i])
+    allBoxes[i].className = 'buttons'
   }
 }
 
-
 document.getElementById('checkloss').addEventListener("click", checkLoss);
 document.getElementById('start').addEventListener("click", playSequence);
+document.getElementById('resetclasses').addEventListener("click", resetClasses);
 
 
 
 
 
-
-
-//run set time out in the for loop
-/*setTimeout(function(){
-  greet("ben", 1)
-})
-
-function delayedGreeting (name, cnt, delay) {
-  setTimeout (function() {
-    greet(name, cnt);
-  }, delay)
-}
-for (var i = 0; i < names.length, i++) {
-  delayedGreeting(names[i], i, i * 1000)
-}
-//clearInterval for sequence
-//
-
-
-//make an array that is static
-//can i track when they click
-//queryselectorall
-*/
