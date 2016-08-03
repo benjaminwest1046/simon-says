5a0f911f420a17dc166e9031c79e799764960e04
Benjamin West <benjaminwest1046@gmail.com>
2016-08-03 11:08:46 -0400

    Added readme file

--- app1.js
+++ app1.js
@@ -1,63 +1,82 @@
-window.onload = function() {
+$(document).ready(function(){
+   console.log("jquery is ready")
+});
+//this was part of the second way
+/*function loadListeners() {
+    console.log("Did this load")
+    for (var i = 0; i < buttonArray.length; i++) {
+      buttonArray[i].addEventListener("click", function() {
+        userClicks.push(this.id)
+        console.log("user clicks: ", userClicks)
+      })
+    }
+  }*/
 
-  var board = {
-    name: "Simon-Says",
-    activeButton: "white",
-  }
-
-}
-var topRight    = document.getElementById("top-right").id
-var topLeft     =  document.getElementById("top-left").id
-var bottomRight = document.getElementById("bottom-right").id
-var bottomLeft  = document.getElementById("bottom-left").id
-var buttons = [topRight, topLeft, bottomRight, bottomLeft]
+var topRight    = document.getElementById("top-right").id;
+var topLeft     =  document.getElementById("top-left").id;
+var bottomRight = document.getElementById("bottom-right").id;
+var bottomLeft  = document.getElementById("bottom-left").id;
+var buttons = [topRight, topLeft, bottomRight, bottomLeft];
 var userClicks = [];
 var sequenceArray = [];
-var buttonClicked = document.getElementsByClassName("buttons");
-var ready = false
+var buttonArray = document.getElementsByClassName("buttons");
+var ready = false;
+var listener = false;
 
 function allowClicks() {
-  userClicks = []
-  for (var i = 0; i < buttonClicked.length; i++) {
-      something();
-      function something () {
-        buttonClicked[i].addEventListener("click", function(){
-        userClicks.push(this.id)
-        console.log("user clicks = ", userClicks, "Length = ", userClicks.length)
-        if (userClicks.length === sequenceArray.length) {
-        checkLoss();
-        }
-        })
-
-      }
-  }
-  }
-  /*for (var i = 0; i < buttonClicked.length; i++) {
-      buttonClicked[i].addEventListener("click", function(){
+  for (var i = 0; i < buttonArray.length; i++) {
+      buttonArray[i].addEventListener("click", function(){
       userClicks.push(this.id)
-      console.log("user clicks = ", userClicks, "Length = ", userClicks.length)
+      console.log("user clicks = ", userClicks, "sequence =  ", sequenceArray)
       if (userClicks.length === sequenceArray.length) {
         checkLoss();
       }
-
       });
 
-  }*/
+  }
+}
+//This was the new method
+/*function allowClicks() {
+  userClicks = []
+  for (var i = 0; i < buttonArray.length; i++) {
+    console.log("removing is called")
+        buttonArray[i].removeEventListener("click", tryThis)
+  }
+  for (var i = 0; i < buttonArray.length; i++) {
+        buttonArray[i].addEventListener("click", tryThis)
+  }
+}
 
+function tryThis() {
+  userClicks.push(this.id)
+        console.log("user clicks = ", userClicks, "Length = ", userClicks.length)
+        if (userClicks.length === sequenceArray.length) {
+          console.log("user clicks", userClicks, "sequenceArray", sequenceArray)
+          checkLoss();
+        }
+}*/
+
+//This was a second way of trying it
+/*function allowClicks() {
+  loadListeners();
+  console.log("user clicks", userClicks.length + "sequence array", sequenceArray.length)
+  if (userClicks.length == sequenceArray.length) {
+    checkLoss();
+  }
+}*/
 function checkLoss () {
   if (ready) {
     for (var i = 0; i < userClicks.length; i++) {
       if (userClicks[i] != sequenceArray[i]) {
         alert("You lose");
         //window.location.reload(false);
-        userClicks = [];
+        //userClicks = [];
         i = 100
       } else {
           setTimeout(function() {
             playSequence();
           }, 500)
-
-          }
+        }
     }
   }
 }
@@ -67,7 +86,6 @@ function delayedPlaySequence(button, delay) {
   var ele = document.getElementById(button);
   setTimeout(function() {
     ele.classList.add("elementToFadeInAndOut");
-    console.log("Delayed sequence here")
   }, delay)
   setTimeout(function() {
     resetClasses();
@@ -75,13 +93,13 @@ function delayedPlaySequence(button, delay) {
 
 }
 
-
 function playSequence() {
     var nextButton = buttons[Math.floor(Math.random() * buttons.length)];
     sequenceArray.push(nextButton)
+    //userClicks = [];
     for (var i = 0; i < sequenceArray.length; i++) {
       delayedPlaySequence(sequenceArray[i], i * 1000);
-  };
+    };
     setTimeout(function() {
       ready = true;
       allowClicks();
---
+++ readme.md
@@ -0,0 +1,19 @@
+#Simon Says
+
+###Current Features/How to Play
+	* Users can start a game
+	* The game will play a sequence by lighting up a button on the board
+	* Players should then click the buttons in order of the played sequence
+	* After each turn, the sequence will grow by 1
+	* If a user clicks the incorrect buttons, an alert will be displayed letting them know they lost
+
+###Trello 
+https://trello.com/b/1XoUz47S/simon-says
+
+###Workflow
+1. Have a static board that displays when players load the page, with a button labeled Start
+2. When the player clicks start, randomly select one of four buttons and add it to an array
+3. Next, play a sequence using the buttons in the array by changing the background opacity and then removing the opacity
+4. Once the sequence is done, listen for clicks and add clicks to a second array. 
+5. Now, compare the sequence array and the user clicks array and if they are the same, add a new button to the sequence array and loop through the sequence again. 
+6. If they arrays do not match, show an alert letting the player know they lost and reset the board
