0d4843b78fff1e7edb7c55f1ffd2c9d3c2c4c800
Benjamin West <benjaminwest1046@gmail.com>
2016-08-03 13:47:53 -0400

    added a round tracker, some styling, and an animation for when the user clicks

--- app1.js
+++ app1.js
@@ -3,6 +3,7 @@ var topRight, topLeft, bottomRight, bottomLeft, buttonArray, buttons;
 var userClickCount = 0;
 var sequenceArray = [];
 var listener = false;
+var round = 0;
 
 // Wait for DOM ready
 $(document).ready(function(){
@@ -17,14 +18,17 @@ $(document).ready(function(){
   buttons = [topRight, topLeft, bottomRight, bottomLeft];
 
   registerClickHandlers();
+
 });
 
 function handleClick(button) {
   console.log('You just clicked', button.id);
   console.log('userClickCount:', userClickCount);
   console.log("sequence =  ", sequenceArray);
+  $(button).fadeOut(100).fadeIn(200)
   if (!checkForMatch(button.id)) {
     alert('You lose!');
+    location.reload();
   }
   else if (userClickCount === sequenceArray.length-1) {
     setTimeout(function() {
@@ -38,7 +42,6 @@ function handleClick(button) {
 }
 
 function registerClickHandlers() {
-  console.log('registerClickHandlers');
   for (var i = 0; i < buttonArray.length; i++) {
       buttonArray[i].addEventListener("click", function() {
         console.log('registering click handler for button', this);
@@ -48,40 +51,10 @@ function registerClickHandlers() {
   document.getElementById('start').addEventListener("click", newGame);
 }
 
-//This was the new method
-/*function allowClicks() {
-  userClicks = []
-  for (var i = 0; i < buttonArray.length; i++) {
-    console.log("removing is called")
-        buttonArray[i].removeEventListener("click", tryThis)
-  }
-  for (var i = 0; i < buttonArray.length; i++) {
-        buttonArray[i].addEventListener("click", tryThis)
-  }
-}
-
-function tryThis() {
-  userClicks.push(this.id)
-        console.log("user clicks = ", userClicks, "Length = ", userClicks.length)
-        if (userClicks.length === sequenceArray.length) {
-          console.log("user clicks", userClicks, "sequenceArray", sequenceArray)
-          checkLoss();
-        }
-}*/
-
-//This was a second way of trying it
-/*function allowClicks() {
-  loadListeners();
-  console.log("user clicks", userClicks.length + "sequence array", sequenceArray.length)
-  if (userClicks.length == sequenceArray.length) {
-    checkLoss();
-  }
-}*/
 function checkForMatch(buttonId) {
   return buttonId == sequenceArray[userClickCount];
 }
 
-//Sequencing
 function delayedPlaySequence(button, delay) {
   var ele = document.getElementById(button);
   setTimeout(function() {
@@ -89,7 +62,7 @@ function delayedPlaySequence(button, delay) {
   }, delay)
   setTimeout(function() {
     resetClasses();
-  }, delay + 100)
+  }, delay + 300)
 
 }
 
@@ -102,7 +75,9 @@ function newGame() {
 function playSequence() {
     var nextButton = buttons[Math.floor(Math.random() * buttons.length)];
     sequenceArray.push(nextButton)
-    //userClicks = [];
+    round = sequenceArray.length;
+    $('#round').text("Round: " + round);
+    console.log(round)
     for (var i = 0; i < sequenceArray.length; i++) {
       delayedPlaySequence(sequenceArray[i], i * 1000);
     };
@@ -114,10 +89,6 @@ function resetClasses() {
     ele.className = "buttons"
   }
 }
-
-//wrap click in a function that checks a variable
-//check each time they click
-//might need to add a delay for variable
 
 
 
--- trash.js
+++ trash.js
@@ -91,46 +91,7 @@ function goToNextButton() {
   var x = 0;
   var ele = document.getElementById(sequenceArray[x]);
   ele.style.background = "white";
-  /*for (i = 0; i < sequenceArray.length; i++) {
-
-    var ele = document.getElementById(sequenceArray[i]);
-        ele.style.background = "white";
-        setInterval(clearButton, 500);*/
-  }
-
-/*,
-    sequence: function() {
-      //var nextButton = board.buttons[Math.floor(Math.random() * board.buttons.length)];
-      //board.sequenceArray.push(nextButton);
-      for (i = 0; i < board.sequenceArray.length; i++) {
-        var ele = document.getElementById(board.sequenceArray[i]);
-        ele.style.background = board.activeButton;
-      }
-    }*/
-
-
-//run set time out in the for loop
-/*setTimeout(function(){
-  greet("ben", 1)
-})
-
-function delayedGreeting (name, cnt, delay) {
-  setTimeout (function() {
-    greet(name, cnt);
-  }, delay)
 }
-for (var i = 0; i < names.length, i++) {
-  delayedGreeting(names[i], i, i * 1000)
-}
-//clearInterval for sequence
-//
-
-
-//make an array that is static
-//can i track when they click
-//queryselectorall
-*/
-
 
 if (ready) {
     console.log(" I am working")
@@ -149,11 +110,6 @@ if (ready) {
   }
 }
 
-
-
-
-
-
 function checkLoss () {
   if (ready){
     for (var i = 0; i < sequenceArray.length; i++) {
@@ -168,8 +124,6 @@ function checkLoss () {
   playSequence();
   }
 }
-
-
 
 document.getElementById('start').addEventListener("click", start());
 
